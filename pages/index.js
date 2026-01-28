17ce278f284af7266b28a03a43046c84c9c88543
Maurício Carvalho dos Santos <100450205+MauricioSS2001@users.noreply.github.com>
2026-01-28 02:12:06 +0000

    Adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return <h1>Pai, Mãe, amo MUITO vocês</h1>
+}
+
+// A exportação padrão é responsável por exportar a estrutura principal, que pode posteriormente conter mais itens como imagens, botões, etc..
+export default Home;
+
+// Para abrir o site na WEB --> Clicar no ícone de antena no canto inferior esquerdo e trocar a propriedade "visibility" para "public".
+// Caso a aplicação seja executada localmente -> Navegador --> localhost:3000
